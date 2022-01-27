ad0ecd2c48496835fc4d8ccfc29a90478ef76a94
Emanoel Kouriat <emanoel.kouriat@student.forsbergsskola.se>
2022-01-27 09:36:28 +0100

    PlayerControllerEK.cs: Added Dash Mechanics

--- Assets/Scripts/EmanoelKouriat/PlayerControllerEK.cs
+++ Assets/Scripts/EmanoelKouriat/PlayerControllerEK.cs
@@ -1,10 +1,15 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class PlayerControllerEK : MonoBehaviour
 {
     public Rigidbody myRigidbody;
     public float moveSpeed = 5f;
     public float jumpForce = 500f;
+    
+    public float _dashTime = 0.1f;
+    public float _dashSpeed = 10f;
 
     private void Update()
     {
@@ -24,5 +29,24 @@ public class PlayerControllerEK : MonoBehaviour
         //Preferably interact with physics in FixedUpdate() 
         if (jumpInput)
             myRigidbody.AddForce(Vector3.up * jumpForce);
+        
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            StartCoroutine(DashCoroutine());
+        }
+    }
+    
+    private IEnumerator DashCoroutine()
+    {
+        float startTime = Time.time; // need to remember this to know how long to dash
+        while (Time.time < startTime + _dashTime)
+        {
+            if (Input.GetKey(KeyCode.D))
+                transform.Translate(transform.right * _dashSpeed * Time.deltaTime);
+            if (Input.GetKey(KeyCode.A))
+                transform.Translate(transform.right * -_dashSpeed * Time.deltaTime);
+            // or controller.Move(...), dunno about that script
+            yield return null; // this will make Unity stop here and continue next frame
+        }
     }
 }
